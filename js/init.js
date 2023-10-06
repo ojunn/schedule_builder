d65fc0b77658fec0839cdde6efe2da765d688296
ojunn <nominanova+github@gmail.com>
2023-10-06 06:31:04 +0000

    Tabulatorを入れた

---
+++ js/init.js
@@ -0,0 +1,21 @@
+//import {Tabulator, FormatModule, EditModule} from 'tabulator-tables';
+
+Tabulator.registerModule([FormatModule, EditModule]);
+
+var table = new Tabulator("#classes-in-event", {
+    columns:[
+        {title:"番号", field:"name"},
+        {title:"種目", field:"class"},
+        {title:"性別", field:"gender"},
+        {title:"級", field:"grade"},
+        {title:"人数", field:"number"},
+        {title:"グループ分け", field:"grouping"},
+        {title:"グループ数", field:"group-number"},
+        {title:"課題", field:"type", hozAlign:"center"},
+        {title:"WU時間", field:"warm-up-time", hozAlign:"center"},
+        {title:"加算時間", field:"off-ice-time", hozAlign:"center"},
+        {title:"所要時間", field:"total-time", hozAlign:"center"},
+        {title:"開始時刻", field:"start-time", hozAlign:"center"},
+        {title:"終了時刻", field:"end-time", hozAlign:"center"},
+    ],
+});
