afe2a32775f8d5496cc4478ff013d76ab2b38847
ojunn <nominanova+github@gmail.com>
2023-10-06 07:32:30 +0000

    サンプルデータを修正

--- js/init.js
+++ js/init.js
@@ -19,7 +19,7 @@ var table = new Tabulator("#classes-in-event", {
         {title:"終了時刻", field:"end_time", hozAlign:"center"},
     ],
     data:[
-        {id: "1", class: "無初級成年女子", gender: "女", grade: "無初級", number: "4", grouping: "4", group_number: "1", type: "JSF0", warm_up_time: "4", off_ice_time: "2", total_time: "3", start_time: "8:00", end_time: "9:00"},
-        {id: "2", class: "", gender: "", grade: "", number: "", grouping: "", group_number: "", type: "", warm_up_time: "", off_ice_time: "", total_time: "", start_time: "", end_time},
+        {id: "1", class: "無初級成年女子", gender: "女", grade: "無初級", number: "4", grouping: "4", group_number: "1", type: "JSF0", skating_time: "1", warm_up_time: "4", off_ice_time: "2", total_time: "3", start_time: "8:00", end_time: "9:00"},
+        {id: "2", class: "", gender: "", grade: "", number: "", grouping: "", group_number: "", type: "", skating_time: "1", warm_up_time: "", off_ice_time: "", total_time: "", start_time: "", end_time: ""},
     ],
 });
